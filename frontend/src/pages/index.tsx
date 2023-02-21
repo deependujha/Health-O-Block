e14e6e47098a5752ccf5640048f3d23371f88a5d
deependujha <deependujha21@gmail.com>
2023-02-21 14:52:44 +0530

    started the project

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='text-center text-blue-700'>Hello world</div>
+    </div>
+  )
+}
+
+export default Home
